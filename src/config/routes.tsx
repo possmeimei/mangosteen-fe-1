626c209ab633ea3a37e05349d39f1943adde0b95
zhangchen <383922689@qq.com>
2023-01-07 11:30:46 +0800

    修改默认路由

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,5 +1,3 @@
-import {Foo} from '../views/Foo';
-import {Bar} from '../views/Bar';
 import {First} from '../components/welcome/first';
 import {Second} from '../components/welcome/second';
 import {Third} from '../components/welcome/third';
@@ -7,10 +5,10 @@ import {Fourth} from '../components/welcome/fourth';
 import {Welcome} from '../views/Welcome';
 
 export const routes = [
-    {path: '/', component: Foo},
-    {path: '/about', component: Bar},
+    {path: '/', redirect:'/welcome'},
     {path: '/welcome', component: Welcome,
         children:[
+            {path:'',redirect:'/welcome/1'},
             {path: '1', component: First},
             {path: '2', component: Second},
             {path: '3', component: Third},
