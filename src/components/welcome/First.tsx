ef85d7ce680a485182fa70bd0c83f2d1cdbd381a
zhangchen <383922689@qq.com>
2023-01-08 16:39:33 +0800

    滑动切换路由

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,12 +1,27 @@
 import s from './WelcomeLayout.module.scss';
-import { FunctionalComponent } from 'vue';
-export const First: FunctionalComponent = () => {
-    return <div class={s.card}>
-        <svg>
-            <use xlinkHref="#piggy"></use>
-        </svg>
-        <p>会挣钱<br />还会省钱</p>
-    </div>
-}
+import {defineComponent, ref, watchEffect} from 'vue';
+import {useSwipe} from '../../hooks/useSwipe';
+import {useRouter} from 'vue-router';
 
-First.displayName = 'First'
+export const First = defineComponent({
+    setup() {
+        const div = ref<HTMLDivElement>();
+        const router = useRouter();
+        const {direction, swiping} = useSwipe(div,{
+            beforeStart:e=>e.preventDefault()
+        });
+        watchEffect(() => {
+            if (swiping.value === true && direction.value === 'left') {
+                return router.push('/welcome/2');
+            }
+        });
+        return () => (
+            <div class={s.card} ref={div}>
+                <svg>
+                    <use xlinkHref="#piggy"></use>
+                </svg>
+                <p>会挣钱<br/>还会省钱</p>
+            </div>
+        );
+    }
+});
