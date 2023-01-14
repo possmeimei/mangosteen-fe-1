15ef6e9324d7701b5e307cace81cc98e672ac25f
zhangchen <383922689@qq.com>
2023-01-14 18:10:05 +0800

    添加vant组件，开始做数字键盘â

--- src/components/Items/InputPad.tsx
+++ src/components/Items/InputPad.tsx
@@ -14,17 +14,35 @@ export const InputPad = defineComponent({
         const now = new Date()
         const refDate = ref<Date>(now)
         const buttons = [
-            {text:'1',onClick:()=>{}},
-            {text:'2',onClick:()=>{}},
-            {text:'3',onClick:()=>{}},
+            {text:'1',onClick:()=>{
+                refAmount.value += 1
+                }},
+            {text:'2',onClick:()=>{
+                    refAmount.value += 2
+                }},
+            {text:'3',onClick:()=>{
+                    refAmount.value += 3
+                }},
             {text:'清空',onClick:()=>{}},
-            {text:'4',onClick:()=>{}},
-            {text:'5',onClick:()=>{}},
-            {text:'6',onClick:()=>{}},
+            {text:'4',onClick:()=>{
+                    refAmount.value += 4
+                }},
+            {text:'5',onClick:()=>{
+                    refAmount.value += 5
+                }},
+            {text:'6',onClick:()=>{
+                    refAmount.value += 6
+                }},
             {text:'+',onClick:()=>{}},
-            {text:'7',onClick:()=>{}},
-            {text:'8',onClick:()=>{}},
-            {text:'9',onClick:()=>{}},
+            {text:'7',onClick:()=>{
+                    refAmount.value += 7
+                }},
+            {text:'8',onClick:()=>{
+                    refAmount.value += 8
+                }},
+            {text:'9',onClick:()=>{
+                    refAmount.value += 9
+                }},
             {text:'-',onClick:()=>{}},
             {text:'.',onClick:()=>{}},
             {text:'0',onClick:()=>{}},
@@ -35,6 +53,7 @@ export const InputPad = defineComponent({
         const showDatePicker = () => refDatePickerVisible.value = true
         const hideDatePicker = () => refDatePickerVisible.value = false
         const setDate = (date: Date) => { refDate.value = date; hideDatePicker() }
+        const refAmount = ref('')
         return () => <>
             <div class={s.output}>
                 <span class={s.date}>
@@ -45,7 +64,7 @@ export const InputPad = defineComponent({
                                   onConfirm={setDate} onCancel={hideDatePicker}/>
                     </Popup>
                 </span>
-                <span class={s.amount}>201.6</span>
+                <span class={s.amount}>{refAmount.value}</span>
             </div>
             <div class={s.buttons}>
                 {buttons.map(button=><button onClick={button.onClick}>{button.text}</button>)}
