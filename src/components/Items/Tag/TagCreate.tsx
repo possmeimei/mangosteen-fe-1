e8f918b76822fdb3b2070239f4d393f58ef8ba65
zhangchen <383922689@qq.com>
2023-02-14 22:36:10 +0800

    准备实现validate函数

--- src/components/Items/Tag/TagCreate.tsx
+++ src/components/Items/Tag/TagCreate.tsx
@@ -1,4 +1,4 @@
-import {defineComponent, PropType, reactive} from 'vue';
+import {defineComponent, PropType, reactive, toRaw} from 'vue';
 import {MainLayout} from '../../../layouts/MainLayout';
 import {Icon} from '../../../shared/Icon';
 import s from './TagCreate.module.scss';
@@ -13,42 +13,52 @@ export const TagCreate = defineComponent({
     },
     setup(props, context) {
         const formData = reactive({
-            name:'',
-            sign:'',
-        })
+            name: '',
+            sign: '',
+        });
+        const onSubmit = (e: Event) => {
+            console.log(toRaw(formData));
+            // const rules = [
+            //     {key: 'name', required: true, message:'必填'},
+            //     {key: 'name', pattern: /^.{1,4}$/, message: '只能填1到4个字'},
+            //     {key: 'sign', required: true},
+            // ];
+            // const errors = validate(formData, rules);
+            e.preventDefault();
+        };
         return () => (
             <MainLayout>{{
                 title: () => '新建标签',
                 icon: () => <Icon name={'return'} onClick={() => {
                 }}/>,
                 default: () => <>
-                        <form action="" class={s.form}>
-                            <div class={s.formRow}>
-                                <label>
-                                    <span class={s.formItem_name}>标签名</span>
-                                    <div class={s.formItem_value}>
-                                        <input type="text" v-model={formData.name}/>
-                                    </div>
-                                    <div class={s.formItem_errorHint}>
-                                        <span>必填</span>
-                                    </div>
-                                </label>
-                            </div>
-                            <div class={s.formRow}>
-                                <label>
-                                    <span class={s.formItem_name}>符号{formData.sign}</span>
-                                    <div>
-                                        <EmojiSelect v-model={formData.sign} class={[s.formItem, s.emojiList, s.error]}/>
-                                    </div>
-                                </label>
-                            </div>
-                            <div class={s.formRow}>
-                                <p class={s.tips}>记账时长按标签，即可进行编辑</p>
-                            </div>
-                            <div class={s.formRow}>
-                                    <Button class={s.button}>确定</Button>
-                            </div>
-                        </form>
+                    <form class={s.form} onSubmit={onSubmit}>
+                        <div class={s.formRow}>
+                            <label>
+                                <span class={s.formItem_name}>标签名</span>
+                                <div class={s.formItem_value}>
+                                    <input type="text" v-model={formData.name}/>
+                                </div>
+                                <div class={s.formItem_errorHint}>
+                                    <span>必填</span>
+                                </div>
+                            </label>
+                        </div>
+                        <div class={s.formRow}>
+                            <label>
+                                <span class={s.formItem_name}>符号{formData.sign}</span>
+                                <div>
+                                    <EmojiSelect v-model={formData.sign} class={[s.formItem, s.emojiList, s.error]}/>
+                                </div>
+                            </label>
+                        </div>
+                        <div class={s.formRow}>
+                            <p class={s.tips}>记账时长按标签，即可进行编辑</p>
+                        </div>
+                        <div class={s.formRow}>
+                            <Button class={s.button}>确定</Button>
+                        </div>
+                    </form>
                 </>
             }}</MainLayout>
         );
