487c78b99a87c4f8d1f4d4a83cd912150e0a2f04
zhangchen <383922689@qq.com>
2023-04-02 01:02:11 +0800

    重构 betterData1

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -21,9 +21,9 @@ export const mockTagEdit: Mock = config => {
 export const mockItemSummary: Mock = config => {
     return [200, {
         "groups": [
-            { "happen_at": "2023-06-18T00:00:00.000+0800", "amount": 100 },
-            { "happen_at": "2023-06-22T00:00:00.000+0800", "amount": 300 },
-            { "happen_at": "2023-06-29T00:00:00.000+0800", "amount": 200 }
+            { "happen_at": "2023-04-21T00:00:00.000+0800", "amount": 100 },
+            { "happen_at": "2023-04-25T00:00:00.000+0800", "amount": 300 },
+            { "happen_at": "2023-04-29T00:00:00.000+0800", "amount": 200 }
         ],
         "summary": 600
     }]
