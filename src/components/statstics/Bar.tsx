84c07d9a2072b625f6779d33dd46d1b52120ff6f
zhangchen <383922689@qq.com>
2023-04-03 23:34:49 +0800

    完成bar chart

--- src/components/statstics/Bar.tsx
+++ src/components/statstics/Bar.tsx
@@ -1,23 +1,15 @@
 import {computed, defineComponent, PropType, reactive} from 'vue';
 import s from './Bar.module.scss';
+import {Money} from '../../shared/Money';
 
 export const Bar = defineComponent({
+    props: {
+        data: Array as PropType<{ tag: Tag, amount: number, percent:number }[]>
+    },
     setup(props, context) {
-        const data3 = reactive([
-            {tag: {id: 1, name: '房租', sign: 'x'}, amount: 2350},
-            {tag: {id: 1, name: '吃饭', sign: 'x'}, amount: 900},
-            {tag: {id: 1, name: '通勤', sign: 'x'}, amount: 600},
-        ]);
-        const betterData3 = computed(() => {
-            const total = data3.reduce((sum, item) => sum + item.amount, 0);
-            return data3.map(item => ({
-                ...item,
-                percent: Math.round(item.amount / total * 100) + '%'
-            }));
-        });
         return () => (
             <div class={s.wrapper}>
-                {betterData3.value.map(({tag, amount, percent}) => {
+                {(props.data && props.data.length > 0) ? props.data.map(({tag, amount, percent}) => {
                     return (
                         <div class={s.topItem}>
                             <div class={s.sign}>
@@ -25,16 +17,17 @@ export const Bar = defineComponent({
                             </div>
                             <div class={s.bar_wrapper}>
                                 <div class={s.bar_text}>
-                                    <span>{tag.name} - {percent}</span>
-                                    <span>￥{amount}</span>
+                                    <span>{tag.name} - {percent}%</span>
+                                    <span>￥<Money value={amount}/></span>
                                 </div>
                                 <div class={s.bar}>
-                                    <div class={s.bar_inner}></div>
+                                    <div class={s.bar_inner} style={{width:`${percent}%`}}></div>
                                 </div>
                             </div>
                         </div>
                     );
-                })}
+                }) : <div>没有数据</div>
+                }
             </div>
         );
     }
